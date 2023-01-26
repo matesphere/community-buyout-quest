99a6b0162073b6aa79b7ba567012c239ef93fed8
David Carter <dccarta@gmail.com>
2023-01-26 00:49:15 +0000

    drop Questy static query in order to deploy successfully

--- packages/shared-ui/components/student/ReadQuesty.tsx
+++ packages/shared-ui/components/student/ReadQuesty.tsx
@@ -1,24 +1,24 @@
 import { FC, ReactNode } from 'react'
-import { graphql, useStaticQuery } from 'gatsby'
-import { GatsbyImage } from 'gatsby-plugin-image'
+// import { graphql, useStaticQuery } from 'gatsby'
+// import { GatsbyImage } from 'gatsby-plugin-image'
 
 export const ReadQuesty: FC<{ text: string | ReactNode }> = ({ text }) => {
-    const data = useStaticQuery(graphql`
-        query {
-            image1: file(relativePath: { eq: "blue-1.jpg" }) {
-                childImageSharp {
-                    gatsbyImageData(layout: CONSTRAINED)
-                }
-            }
-        }
-    `)
+    // const data = useStaticQuery(graphql`
+    //     query {
+    //         image1: file(relativePath: { eq: "blue-1.jpg" }) {
+    //             childImageSharp {
+    //                 gatsbyImageData(layout: CONSTRAINED)
+    //             }
+    //         }
+    //     }
+    // `)
     return (
         <div className="blue-holder-border questies-holder">
             <div className="small-questies">
-                <GatsbyImage
+                {/* <GatsbyImage
                     alt=""
                     image={data.image1.childImageSharp.gatsbyImageData}
-                />
+                /> */}
             </div>
             <p className="sm-type-lead small-questies-holder">{text}</p>
         </div>
