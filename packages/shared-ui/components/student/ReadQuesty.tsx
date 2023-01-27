3c25ef167517426211416dc75ea5272800e5b43b
stuarthull <stuartthull@gmail.com>
2023-01-27 10:05:39 +0000

    chore: add questy directly

--- packages/shared-ui/components/student/ReadQuesty.tsx
+++ packages/shared-ui/components/student/ReadQuesty.tsx
@@ -1,24 +1,11 @@
 import { FC, ReactNode } from 'react'
-// import { graphql, useStaticQuery } from 'gatsby'
-// import { GatsbyImage } from 'gatsby-plugin-image'
+import bluequesty from './blue-1.jpg'
 
 export const ReadQuesty: FC<{ text: string | ReactNode }> = ({ text }) => {
-    // const data = useStaticQuery(graphql`
-    //     query {
-    //         image1: file(relativePath: { eq: "blue-1.jpg" }) {
-    //             childImageSharp {
-    //                 gatsbyImageData(layout: CONSTRAINED)
-    //             }
-    //         }
-    //     }
-    // `)
     return (
         <div className="blue-holder-border questies-holder">
             <div className="small-questies">
-                {/* <GatsbyImage
-                    alt=""
-                    image={data.image1.childImageSharp.gatsbyImageData}
-                /> */}
+                <img src={bluequesty} alt="" />
             </div>
             <p className="sm-type-lead small-questies-holder">{text}</p>
         </div>
