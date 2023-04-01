101dd5f8c34733f3a1b175629eb9883fd76bea2d
stuarthull <stuartthull@gmail.com>
2023-04-01 19:01:10 +0100

    chore: scale size, questies flex narrow width

--- apps/urban-quest/src/pages/student/stages/stage-1/stage-1-landing.tsx
+++ apps/urban-quest/src/pages/student/stages/stage-1/stage-1-landing.tsx
@@ -92,18 +92,17 @@ const Stage1LandingPage: FC = () => {
                             <ReadQuesty text={stageIntro} />
 
                             <div className="homepage-image mb-4">
-                                <div class="embed-container">
-                                  <div class="embed-responsive">
+                                  <div class="video-responsive">
                                     <iframe
+                                        class="video-responsive-item"
                                         src="https://player.vimeo.com/video/321444139?h=378fb67b89"
-                                        width="100%"
+                                        width="640"
                                         height="360"
                                         frameBorder="0"
                                         allow="autoplay; fullscreen; picture-in-picture"
                                         allowFullScreen
                                     ></iframe>
                                     </div>
-                                </div>
                                 <p>
                                     Midsteeple Quarter - Dumfries High Street
                                     from Community Land Scotland.
